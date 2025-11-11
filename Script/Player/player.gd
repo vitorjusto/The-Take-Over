001bac06d25d07814aaea57c968e099321e6c9fb
vitorjusto <vitorhugojusto1@gmail.com>
2025-11-11 16:38:07 -0300

    create player projectile

--- Script/Player/player.gd
+++ Script/Player/player.gd
@@ -1,35 +1,61 @@
 extends CharacterBody2D
 
+enum Direction {LEFT, RIGHT, UP, DOWN}
 
 const SPEED = 300.0
 const RUNNING_SPEED = 900.0
-const JUMP_VELOCITY = -400.0
-const GRAVITY = 800
+const JUMP_VELOCITY = -800.0
+var facingDirection: Direction = Direction.RIGHT
+
+@export var LevelManager: Node2D
+
+@onready var projectile: PackedScene = load("res://Scenes/Player/PlayerProjectile.tscn")
 
 func _physics_process(delta: float) -> void:
 	
 	HandleJump(delta)
-	HandleMoviment(delta)
+	HandleMoviment()
+	HandleShoot()
 	
 	move_and_slide()
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 
 
 
 func HandleJump(delta: float) -> void:
 		# Add the gravity.
 	if not is_on_floor():
-		velocity.y += GRAVITY * delta
+		velocity += get_gravity() * delta
 
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
-func HandleMoviment(delta: float) -> void:
+func HandleMoviment() -> void:
 	var direction := Input.get_vector("Left", "Right", "Up", "Down")
-	
+	if direction.x > 0:
+		facingDirection = Direction.RIGHT
+	elif direction.x < 0:
+		facingDirection = Direction.LEFT
+		
 	velocity.x = move_toward(velocity.x, direction.x * (RUNNING_SPEED if Input.is_action_pressed("Run") else SPEED), 40)
 
-
+func HandleShoot() -> void:
+	if not Input.is_action_just_pressed("Shoot"):
+		return
+	
+	var proj: PlayerProjectile = projectile.instantiate();
+	
+	if facingDirection == Direction.LEFT:
+		proj.xSpeed *= -1
+	if Input.is_action_pressed("Up"):
+		proj.JumpVelocity = proj.MAX_JUMP_VELOCITY
+		proj.velocity = Vector2(0, proj.JumpVelocity)
+	
+	elif Input.is_action_pressed("Down"):
+		proj.JumpVelocity = proj.MIN_JUMP_VELOCITY
+	
+	proj.position = position
+	
+	LevelManager.add_child(proj)
+	
 func OnEnemyDetected(body: Node2D) -> void:
 	print("doeo")
---
+++ Script/Player/playerProjectile.gd
@@ -0,0 +1,29 @@
+class_name PlayerProjectile
+extends CharacterBody2D
+
+var xSpeed = 500.0
+var JumpVelocity = -500.0
+const MAX_JUMP_VELOCITY = -900
+const MIN_JUMP_VELOCITY = -100
+
+func _physics_process(delta: float) -> void:
+	var jumpVelocityModifier = 0;
+	
+	if(Input.is_action_pressed("Up")):
+		jumpVelocityModifier -= 5
+	elif(Input.is_action_pressed("Down")):
+		jumpVelocityModifier += 5
+	
+	JumpVelocity = clamp(JumpVelocity + jumpVelocityModifier, MAX_JUMP_VELOCITY, MIN_JUMP_VELOCITY)
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	else:
+		velocity.y += JumpVelocity
+	
+	velocity = Vector2(xSpeed, clamp(velocity.y, -1500, 7000))
+	
+	if is_on_wall():
+		xSpeed *= -1
+	
+	move_and_slide()
