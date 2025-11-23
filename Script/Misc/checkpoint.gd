a5939e2813895bc39543b2613f8945b6107bdbac
vitorjusto <vitorhugojusto1@gmail.com>
2025-11-23 13:41:24 -0300

    create sprite checkpoint

--- Script/Misc/checkpoint.gd
+++ Script/Misc/checkpoint.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+
 func onPlayerDeteced(body: Node2D) -> void:
 	var player : Player = body
 	player.checkpointPosition = position 
+	var ani : AnimatedSprite2D = get_node("AnimatedSprite2D")
+	ani.play("Active")
+	
+	var aniPlayer : AnimationPlayer = get_node("AnimationPlayer")
+	aniPlayer.play("active")
