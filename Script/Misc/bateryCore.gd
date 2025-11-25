5a928f658c56da3526b45507e7ae5bfcddffca5f
vitorjusto <vitorhugojusto1@gmail.com>
2025-11-24 22:46:50 -0300

    create core sprite

--- Script/Misc/bateryCore.gd
+++ Script/Misc/bateryCore.gd
@@ -1,9 +1,11 @@
 extends StaticBody2D
 var hp = 10
+@onready var ani : AnimationPlayer = get_node("AnimationPlayer2")
 
 func onPlayerProjectileDeteced(body: Node2D) -> void:
 	body.call_deferred("queue_free")
 	
+	ani.play("new_animation")
 	hp-= 1
 	if hp > 0:
 		return
